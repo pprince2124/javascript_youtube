5680933a31df7c29964de67867ec5aa34b281f8c
Prince Singh <princesingh9810877876@gmail.com>
2024-08-31 12:46:03 +0000

    0.1variable

---
+++ .devcontainer/01_basics/0.1variable.js
@@ -0,0 +1,5 @@
+const AccountId = 1234
+let accountemail = "prince@gmail.com"
+var accountPassword = "110011"
+accountState = "punjab"
+console.table([AccountId,accountemail,accountPassword,accountState])
